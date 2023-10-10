aaa984ea19acaac672620c2b5c28e47fd6bac0fe
ccsunnyfd <chzhyfd@126.com>
2023-10-10 10:01:25 +0800

    [fix]PieChart display hidden problem

--- app/(dashboard)/ExpenseBreakdownByCategory.tsx
+++ app/(dashboard)/ExpenseBreakdownByCategory.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useMemo } from 'react'
-import { PieChart, Pie, Cell } from 'recharts'
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
 import BoxHeader from './components/BoxHeader'
 import { GetKpisResponse } from '../kpi/types'
 
@@ -36,20 +36,22 @@ const ExpenseBreakdownByCategory = ({ data }: Props) => {
       <div className="flex-between mt-2 h-full w-full gap-2 p-[0.1rem] text-center">
         {pieChartData?.map((data, i) => (
           <div className="h-full w-full" key={`${data[0].name}-${i}`}>
-            <PieChart width={110} height={100}>
-              <Pie
-                stroke="none"
-                data={data}
-                innerRadius={18}
-                outerRadius={35}
-                paddingAngle={2}
-                dataKey="value"
-              >
-                {data.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={pieColors[index]} />
-                ))}
-              </Pie>
-            </PieChart>
+            <ResponsiveContainer width="70%" height="50%">
+              <PieChart width={110} height={100}>
+                <Pie
+                  stroke="none"
+                  data={data}
+                  innerRadius={18}
+                  outerRadius={35}
+                  paddingAngle={2}
+                  dataKey="value"
+                >
+                  {data.map((_, index) => (
+                    <Cell key={`cell-${index}`} fill={pieColors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
             <h5>{data[0].name}</h5>
           </div>
         ))}
